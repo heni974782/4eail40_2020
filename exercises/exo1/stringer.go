2cb9ea510af0031606517842b0e35381777b045f
rob <195286@ecam.be>
2020-10-11 19:19:42 +0200

    ajout exo1

--- exercises/exo1/stringer.go
+++ exercises/exo1/stringer.go
@@ -1,8 +1,40 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+	"math"
+)
 
 // Implement types Rectangle, Circle and Shape
+type Rectangle struct{
+	width, length float64
+}
+
+type Circle struct{
+	radius float64
+}
+
+type Shape interface {
+	Surface() float64
+}
+
+func (r Rectangle) Surface() float64 {
+    return r.width * r.length
+}
+
+func (c Circle) Surface() float64 {
+    return math.Pi * c.radius * c.radius
+}
+
+func (r Rectangle) String() string {
+    return fmt.Sprintf("Square of width %v and length %v", r.width, r.length)
+}
+
+func (c Circle) String() string {
+    return fmt.Sprintf("Circle of radius %v",c.radius)
+}
+
+
 
 func main() {
 	r := &Rectangle{2, 3}
