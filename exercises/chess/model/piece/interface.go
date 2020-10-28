54cfff25d5baf1101c1fbc1b6e9374a89513f78c
rob <195286@ecam.be>
2020-10-28 20:47:46 +0100

    ajout v2 chess

---
+++ exercises/chess/model/piece/interface.go
@@ -0,0 +1,17 @@
+// Package piece will handle game pieces.
+package piece
+
+import (
+	"fmt"
+	"github.com/chess/model/coord"
+	"github.com/chess/model/player"
+)
+
+// Piece represents a game piece
+type Piece interface {
+	fmt.Stringer
+	// Color returns the appartenance.
+	Color() player.Color
+	// Moves returns a set of valid move.
+	Moves(isCapture bool) map[coord.ChessCoordinates]bool
+}
