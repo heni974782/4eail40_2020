2ec34ef33e65ff6a240c8fc6b7acb3c10b4a127b
heni974782 <73293817+heni974782@users.noreply.github.com>
2020-10-30 11:48:50 +0100

    Update piece.go

--- exercises/chess/model/piece.go
+++ exercises/chess/model/piece.go
@@ -3,39 +3,3 @@ package model
 //TODO Implement game pieces here
 package piece
 
-import (
-	"fmt"
-	"github.com/heni974782/4eail40_2020/exercises/chess/chess/model/coord"
-	"github.com/heni974782/4eail40_2020/exercises/chess/chess/model/player"
-)
-
-type EchecPiece struct {
-	coord coord.Cartesian
-	color player.Color
-}
-
-func NewEchecPiece(coord coord.Cartesian, color player.Color) EchecPiece {
-	return EchecPiece{coord, color}
-}
-
-func (e EchecPiece) Color() player.Color {
-	return e.color
-}
-
-func (e EchecPiece) Moves(isCapture bool) map[coord.ChessCoordinates]bool {
-	return map[coord.ChessCoordinates]bool{}
-}
-func (e EchecPiece) String() string {
-	return fmt.Sprintf("coord : {%d}, color : %d", e.coord, e.color)
-}
-
-func (e EchecPiece) Coord(n int) (int, error) {
-	switch n {
-	case 0:
-		return 0, nil
-	case 1:
-		return 1, nil
-
-	}
-	return 0, fmt.Errorf("unknown coord component %d", n)
-}
